c9ab8b50ed9ed8059f60713f61a8ec0498ee4cec
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-09-11 18:35:30 +0200

    Tutorial checkpoint

--- SortedSetDemo/Program.cs
+++ SortedSetDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SortedSetDemo
 {
@@ -6,7 +7,28 @@ namespace SortedSetDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //sorted sets protects against duplicate values aswell
+            var bigCities = new SortedSet<string>
+                            //customer comparer implementation
+                            (new UncasedStringComparer())
+            { "New York", "Manchester", "Sheffield", "Paris" };
+
+            bigCities.Add("SHEFFIELD");
+            bigCities.Add("Beijing");
+
+            foreach (string city in bigCities)
+                Console.WriteLine(city);
+        }
+    }
+
+
+    //customer comparer for SortedSet must implement IComparer not IEquality
+    class UncasedStringComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            return string.Compare(x, y,
+                StringComparison.CurrentCultureIgnoreCase);
         }
     }
 }
